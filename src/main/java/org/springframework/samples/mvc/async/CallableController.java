222a1224e3a34354a860c7382638f5cdb285044e
unknown <vikasy@vikasy.cybage.com>
2015-01-09 15:59:57 +0530

    test2

--- src/main/java/org/springframework/samples/mvc/async/CallableController.java
+++ src/main/java/org/springframework/samples/mvc/async/CallableController.java
@@ -1,5 +1,4 @@
 package org.springframework.samples.mvc.async;
-
 import java.util.concurrent.Callable;
 
 import org.springframework.stereotype.Controller;
@@ -9,75 +8,64 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.async.WebAsyncTask;
-
 @Controller
 @RequestMapping("/async/callable")
 public class CallableController {
-
-
+	public static final int WAIT =2000,CALLABLE=1000;
+	
 	@RequestMapping("/response-body")
-	public @ResponseBody Callable<String> callable() {
-
+	 @ResponseBody public Callable<String> callable() {
 		return new Callable<String>() {
 			@Override
 			public String call() throws Exception {
-				Thread.sleep(2000);
+				Thread.sleep(WAIT);
 				return "Callable result";
 			}
 		};
 	}
-
 	@RequestMapping("/view")
 	public Callable<String> callableWithView(final Model model) {
-
 		return new Callable<String>() {
 			@Override
 			public String call() throws Exception {
-				Thread.sleep(2000);
+				Thread.sleep(WAIT);
 				model.addAttribute("foo", "bar");
 				model.addAttribute("fruit", "apple");
 				return "views/html";
 			}
 		};
 	}
-
 	@RequestMapping("/exception")
-	public @ResponseBody Callable<String> callableWithException(
+	 @ResponseBody public Callable<String> callableWithException(
 			final @RequestParam(required=false, defaultValue="true") boolean handled) {
-
 		return new Callable<String>() {
 			@Override
 			public String call() throws Exception {
-				Thread.sleep(2000);
+				Thread.sleep(WAIT);
 				if (handled) {
 					// see handleException method further below
 					throw new IllegalStateException("Callable error");
-				}
-				else {
+				}else
+				{
 					throw new IllegalArgumentException("Callable error");
 				}
 			}
 		};
 	}
-
 	@RequestMapping("/custom-timeout-handling")
-	public @ResponseBody WebAsyncTask<String> callableWithCustomTimeoutHandling() {
-
+	 @ResponseBody public WebAsyncTask<String> callableWithCustomTimeoutHandling() {
 		Callable<String> callable = new Callable<String>() {
 			@Override
 			public String call() throws Exception {
-				Thread.sleep(2000);
+				Thread.sleep(WAIT);
 				return "Callable result";
 			}
 		};
-
-		return new WebAsyncTask<String>(1000, callable);
+		return new WebAsyncTask<String>(CALLABLE, callable);
 	}
-
 	@ExceptionHandler
 	@ResponseBody
 	public String handleException(IllegalStateException ex) {
 		return "Handled exception: " + ex.getMessage();
 	}
-
 }
